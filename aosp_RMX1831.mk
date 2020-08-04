945782fb226dd6844e88f50c7c76d940d29804dc
devil-black-786 <60068093+devil-black-786@users.noreply.github.com>
2020-08-04 09:56:54 +0530

    Adde Maintainer Name and build type official

--- aosp_RMX1831.mk
+++ aosp_RMX1831.mk
@@ -54,5 +54,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
 
+# Build type
+CUSTOM_BUILD_TYPE:= OFFICIAL
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fusion.maintainer=Fahim Ahmad
+
 # Apex
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
